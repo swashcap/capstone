e8f7bbded5862dd588ef23e447eaf6d309aa09eb
Cory Reed <swashcap@gmail.com>
2014-09-12 19:44:08 -0700

    Tabs to spaces.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+    $content_width = 640; /* pixels */
 }
 
 if ( ! function_exists( 'capstone_setup' ) ) :
@@ -22,44 +22,44 @@ if ( ! function_exists( 'capstone_setup' ) ) :
  */
 function capstone_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on capstone, use a find and replace
-	 * to change 'capstone' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'capstone', get_template_directory() . '/languages' );
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on capstone, use a find and replace
+     * to change 'capstone' to the name of your theme in all the template files
+     */
+    load_theme_textdomain( 'capstone', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	add_theme_support( 'post-thumbnails' );
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+     */
+    add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'capstone' ),
-	) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'capstone' ),
+    ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support( 'html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+    ) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
-	) );
+    /*
+     * Enable support for Post Formats.
+     * See http://codex.wordpress.org/Post_Formats
+     */
+    add_theme_support( 'post-formats', array(
+        'aside', 'image', 'video', 'quote', 'link',
+    ) );
 }
 endif; // capstone_setup
 add_action( 'after_setup_theme', 'capstone_setup' );
@@ -70,15 +70,15 @@ add_action( 'after_setup_theme', 'capstone_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function capstone_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'capstone' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+    register_sidebar( array(
+        'name'          => __( 'Sidebar', 'capstone' ),
+        'id'            => 'sidebar-1',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
 }
 add_action( 'widgets_init', 'capstone_widgets_init' );
 
@@ -86,11 +86,11 @@ add_action( 'widgets_init', 'capstone_widgets_init' );
  * Enqueue scripts and styles.
  */
 function capstone_scripts() {
-	wp_enqueue_style( 'capstone-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'capstone-style', get_stylesheet_uri() );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'capstone_scripts' );
 
