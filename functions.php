4626a898472480fcbf89857dab0400721f263a76
Cory Reed <swashcap@gmail.com>
2014-09-12 20:19:18 -0700

    Hook up styles and scripts to theme.

--- functions.php
+++ functions.php
@@ -86,7 +86,20 @@ add_action( 'widgets_init', 'capstone_widgets_init' );
  * Enqueue scripts and styles.
  */
 function capstone_scripts() {
-    wp_enqueue_style( 'capstone-style', get_stylesheet_uri() );
+    wp_enqueue_style(
+        'capstone-styles',
+        get_template_directory_uri() . '/assets/css/main.css',
+        array(),
+        null
+    );
+
+    wp_enqueue_script(
+        'capstone-scripts',
+        get_template_directory_uri() . '/assets/js/main.min.js',
+        array('jquery'),
+        null,
+        true
+    );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
