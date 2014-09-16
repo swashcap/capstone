036082d57092b9d444ad1591f2b57a6a2a6d0752
Cory Reed <swashcap@gmail.com>
2014-09-15 23:47:27 -0700

    Add widget areas to the footer.

--- footer.php
+++ footer.php
@@ -12,6 +12,33 @@
 
             <footer class="site__footer" role="contentinfo">
                 <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-md-push-9">
+                            <a href="#content" class="sr-only"><?php _e('Back to content', 'capstone'); ?></a>
+                            <nav id="navigation" role="navigation">
+                                <h2 class="sr-only"><?php _e('Navigation Menu', 'capstone'); ?></h2>
+                                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+                                <?php
+                                    wp_nav_menu(array(
+                                        'menu'           => 'primary',
+                                        'theme_location' => 'primary',
+                                        'depth'          => 2,
+                                        'container'      => false,
+                                        'menu_class'     => 'nav nav-pills nav-stacked'
+                                    ));
+                                ?>
+                            </nav><!-- #navigation -->
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-md-pull-3">
+                            <?php dynamic_sidebar('footer-sidebar-1'); ?>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-md-pull-3">
+                            <?php dynamic_sidebar('footer-sidebar-2'); ?>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-md-pull-3">
+                            <?php dynamic_sidebar('footer-sidebar-3'); ?>
+                        </div>
+                    </div><!-- .row -->
                     <small class="copyright">
                         <?php
                             _e(sprintf('&copy;%d, %s. All rights reserved.', date('Y'), get_bloginfo('name')), 'capstone');
--- functions.php
+++ functions.php
@@ -70,15 +70,42 @@ add_action( 'after_setup_theme', 'capstone_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function capstone_widgets_init() {
-    register_sidebar( array(
-        'name'          => __( 'Sidebar', 'capstone' ),
+    register_sidebar(array(
+        'name'          => __('Sidebar', 'capstone'),
         'id'            => 'sidebar-1',
         'description'   => '',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget'  => '</aside>',
         'before_title'  => '<h1 class="widget-title">',
         'after_title'   => '</h1>',
-    ) );
+    ));
+    register_sidebar(array(
+        'name'          => __('Footer Area 1', 'capstone'),
+        'id'            => 'footer-sidebar-1',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ));
+    register_sidebar(array(
+        'name'          => __('Footer Area 2', 'capstone'),
+        'id'            => 'footer-sidebar-2',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ));
+    register_sidebar(array(
+        'name'          => __('Footer Area 3', 'capstone'),
+        'id'            => 'footer-sidebar-3',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ));
 }
 add_action( 'widgets_init', 'capstone_widgets_init' );
 
