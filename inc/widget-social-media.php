46d8ad26a9b48a60b42d456b29bf6657d5203f9f
Cory Reed <swashcap@gmail.com>
2014-10-05 12:11:47 -0700

    Add title and content area to social media widget.

--- inc/widget-social-media.php
+++ inc/widget-social-media.php
@@ -20,6 +20,9 @@ class Capstone_Widget_Social_Media extends WP_Widget
             array(
                 'classname'   => 'widget_capstone_social_media',
                 'description' => __('Add social media links output as theme buttons', 'capstone')
+            ),
+            array(
+                'width' => 300
             )
         );
     }
@@ -35,10 +38,23 @@ class Capstone_Widget_Social_Media extends WP_Widget
     {
         extract($args);
 
+        $title = apply_filters('widget_title', $instance['title']);
+        $content = $instance['content'];
         $item_output = '<li class="%1$s"><a href="%2$s" title="%3$s">%4$s</a></li>';
 
         if (! empty($instance)) :
             echo $before_widget;
+
+            if (! empty($title)) {
+                echo $before_title . $title . $after_title;
+            }
+
+            echo '<div class="widget-content">';
+
+            if (! empty($content)) {
+                echo '<p>' . $instance['content'] . '</p>';
+            }
+
             echo '<ul>';
 
             foreach (self::get_services() as $slug => $title) {
@@ -54,6 +70,7 @@ class Capstone_Widget_Social_Media extends WP_Widget
             }
 
             echo '</ul>';
+            echo '</div><!-- .widget-content -->';
             echo $after_widget;
         endif; // ! empty($instance)
     }
@@ -66,9 +83,16 @@ class Capstone_Widget_Social_Media extends WP_Widget
      */
     public function form($instance)
     {
+        $title = ! empty($instance['title']) ? strip_tags($instance['title']) : '';
+        $content = ! empty($instance['content']) ? esc_html($instance['content']) : '';
         $services = self::get_services();
 
         ?>
+            <p>
+                <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+                <input type="text" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($title); ?>" id="<?php echo $this->get_field_id('title'); ?>" class="widefat">
+            </p>
+                        <textarea name="<?php echo $this->get_field_name('content'); ?>" id="<?php echo $this->get_field_id('content'); ?>" class="widefat" rows="8" cols="20"><?php echo $content; ?></textarea>
             <p><small><?php _e('Please enter full <abbr>URL</abbr>s to your profile for each social media service.', 'capstone'); ?></small></p>
         <?php
         foreach ($services as $slug => $title) :
@@ -95,6 +119,9 @@ class Capstone_Widget_Social_Media extends WP_Widget
     public function update($new_instance, $old_instance)
     {
         $instance = array();
+
+        $instance['title'] = ! empty($new_instance['title']) ? strip_tags($new_instance['title']) : '';
+        $instance['content'] = ! empty($new_instance['content']) ? esc_html($new_instance['content']) : '';
 
         foreach (self::get_services() as $slug => $title) {
             if (isset($new_instance[$slug]) && ! empty($new_instance[$slug])) {
