5d19f137435c2cc815e4da8f8c12dac0403a7541
Cory Reed <swashcap@gmail.com>
2014-10-25 16:18:54 -0700

    Change generic page layout.

--- page.php
+++ page.php
@@ -7,24 +7,24 @@
 
 get_header(); ?>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                <main class="site__content" role="main">
-                    <?php
-                        while (have_posts()) :
-                            the_post();
+    <main class="site__content container-fluid" role="main">
+        <?php
+            while (have_posts()) :
+                the_post();
 
-                            get_template_part('templates/partials/content', 'page');
+                get_template_part('templates/partials/content', 'page');
 
-                            if (comments_open() || get_comments_number() != '0') :
-                                comments_template();
-                            endif;
-                        endwhile; // have_posts()
-                    ?>
-                </main><!-- .site__content -->
-            </div>
-        </div><!-- .row -->
-    </div><!-- .container-fluid -->
+                if (comments_open() || get_comments_number() != '0') :
+        ?>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-md-6 col-md-push-1">
+                    <?php comments_template(); ?>
+                </div>
+            </div><!-- .row -->
+        <?php
+                endif; // comments_open() || get_comments_number() != '0'
+            endwhile; // have_posts()
+        ?>
+    </main><!-- .site__content.container-fluid -->
 
 <?php get_footer(); ?>
--- templates/partials/content-page.php
+++ templates/partials/content-page.php
@@ -6,26 +6,41 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    </header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('article'); ?>>
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 col-sm-offset-0 col-sm-push-8">
+            <?php if (has_post_thumbnail()) : ?>
+                <div class="entry-thumbnail">
+                    <?php the_post_thumbnail('full'); ?>
+                </div><!-- .entry-thumbnail -->
+            <?php endif; ?>
+            <div class="hidden-xs">
+                <?php get_template_part('templates/partials/about/about', 'testimonial'); ?>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-pull-4 col-md-pull-3">
+            <header class="entry-header">
+                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            </header><!-- .entry-header -->
 
-    <div class="entry-content">
-        <?php the_content(); ?>
-        <?php
-            wp_link_pages( array(
-                'before' => '<div class="page-links">' . __( 'Pages:', 'capstone' ),
-                'after'  => '</div>',
-            ) );
-        ?>
-    </div><!-- .entry-content -->
-
-    <footer class="entry-footer">
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php
+                    wp_link_pages( array(
+                        'before' => '<div class="page-links">' . __( 'Pages:', 'capstone' ),
+                        'after'  => '</div>',
+                    ) );
+                ?>
+            </div><!-- .entry-content -->
+        </div>
+    </div><!-- .row -->
+    <footer class="entry-footer row">
         <div class="hidden">
             <?php capstone_posted_on(); ?>
             <?php capstone_entry_author(); ?>
         </div>
-        <?php edit_post_link( __( 'Edit', 'capstone' ), '<span class="edit-link">', '</span>' ); ?>
+        <div class="col-xs-12 col-sm-8 col-md-6 col-md-offset-1">
+            <?php edit_post_link( __( 'Edit', 'capstone' ), '<span class="edit-link">', '</span>' ); ?>
+        </div>
     </footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+</article><!-- #post-<?php the_ID(); ?> -->
