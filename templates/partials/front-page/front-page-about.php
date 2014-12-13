f0d7c43f062a2abb9e3fd86a9b3e8ede7183b894
Cory Reed <swashcap@gmail.com>
2014-12-13 13:09:44 -0800

    Add admin-controllable link to front page's 'About' section.

--- templates/partials/front-page/front-page-about.php
+++ templates/partials/front-page/front-page-about.php
@@ -6,6 +6,7 @@
  */
 
 if (function_exists('get_field')) :
+    $link = get_field('about_link');
     $image = get_field('about_image');
     $heading = get_field('about_heading');
     $content = get_field('about_content');
@@ -13,23 +14,27 @@ if (function_exists('get_field')) :
 ?>
     <section class="front-page__about">
         <div class="container-fluid">
-            <a href="/about/">
-                <?php capstone_picture($image); ?>
-                <div class="front-page__section__content">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                            <h1><?php echo $heading; ?></h1>
-                            <p class="lead"><?php echo $content; ?></p>
-                            <?php if ($button_text) : ?>
-                                <span class="btn btn-primary"><?php echo $button_text; ?></span>
-                            <?php endif; ?>
-                        </div>
-                    </div><!-- .row -->
-                </div><!-- .front-page__section__content -->
-            </a>
+            <?php if ($link) : ?>
+                <a href="<?php echo esc_url($link); ?>" rel="bookmark">
+            <?php endif; ?>
+            <?php capstone_picture($image); ?>
+            <div class="front-page__section__content">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                        <h1><?php echo $heading; ?></h1>
+                        <p class="lead"><?php echo $content; ?></p>
+                        <?php if ($link && $button_text) : ?>
+                            <span class="btn btn-primary"><?php echo $button_text; ?></span>
+                        <?php endif; ?>
+                    </div>
+                </div><!-- .row -->
+            </div><!-- .front-page__section__content -->
+            <?php if ($link) : ?>
+                </a>
+            <?php endif; ?>
         </div><!-- .container-fluid -->
     </section><!-- .front-page__about -->
 <?php
-    unset($image, $heading, $content, $button_text);
+    unset($link, $image, $heading, $content, $button_text);
 endif; // function_exists('get_field')
 ?>
