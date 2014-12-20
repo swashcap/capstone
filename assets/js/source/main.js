e3943c4ff63b5cdbb4b9d6d681436906a6b84107
Cory Reed <swashcap@gmail.com>
2014-12-20 15:56:36 -0800

    Add script to center front page sections' images.

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function (window, $) {
     'use strict';
 
     /**
@@ -17,11 +17,50 @@
      */
     FastClick.attach(document.body);
 
-    // $($navbarToggle.data('target')).on('show.bs.collapse', function () {
-    //     $(this).addClass('is-open');
-    // });
-    // $($navbarToggle.data('target')).on('hide.bs.collapse', function () {
-    //     $(this).removeClass('is-open');
-    // });
+    /**
+     * Center front page sections' images.
+     */
+    var SCREEN_SMALL = 600;
+    var $window = $(window);
+    var $frontPageSections = $('.front-page__work, .front-page__about, .front-page__blog, .front-page__contact');
+    var centerImage = function ($image) {
+        var $parent = $image.parent();
+        var parentWidth = $parent.width();
+        var parentHeight = $parent.height();
+        var imageWidth = $image.get(0).naturalWidth || $image.width();
+        var imageHeight = $image.get(0).naturalHeight || $image.height();
 
-}(jQuery));
+        if ($window.width() >= SCREEN_SMALL) {
+            if (parentWidth / parentHeight > imageWidth / imageHeight) {
+                $image.css({
+                    height: parentWidth * imageHeight / imageWidth,
+                    left: 0,
+                    position: 'relative',
+                    top: -0.5 * (parentWidth * imageHeight / imageWidth - parentHeight),
+                    width: parentWidth
+                });
+            } else {
+                $image.css({
+                    height: parentHeight,
+                    left: -0.5 * (parentHeight * imageWidth / imageHeight - parentWidth),
+                    position: 'relative',
+                    top: 0,
+                    width: parentHeight * imageWidth / imageHeight
+                });
+            }
+        } else {
+            $image.removeAttr('style');
+        }
+    };
+
+    $.each($frontPageSections, function () {
+        var $image = $(this).find('img');
+
+        if ($image.length) {
+            centerImage($image);
+            $window.resize($.throttle(60, function () {
+                centerImage($image);
+            }));
+        }
+    });
+})(window, jQuery);
